3212f19030943074d1ae4e0fd9af8caf16e0093f
SimphiweGithub <simphiwe0105@gmail.com>
2025-04-17 15:14:36 +0200

    XML WITH C#

---
+++ ReadXML.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+
+class ReadXML
+    {
+    static void Main(string[] args)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/new 1.xml")
+);
+
+        
+        XmlNodeList bookNodes = doc.SelectNodes("/Data/Item");
+
+        foreach (XmlNode bookNode in bookNodes)
+        {
+            string name = bookNode.SelectSingleNode("Name").InnerText;
+            int quantity = int.Parse(bookNode.SelectSingleNode("Quantity").InnerText);
+            double price = double.Parse(bookNode.SelectSingleNode("Price").InnerText);
+
+            Console.WriteLine($"Name: {name}, Quantity: {quantity}, Price: {price}");
+        }
+    }
+}
---
+++ WriteXML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Learning_unit_4_xml
+{
+    class WriteXML
+    {
+        public static void main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(xmlDeclaration);
+
+            XmlElement root = doc.CreateElement("Data");
+            doc.AppendChild(root);
+
+            XmlElement item = doc.CreateElement("Item");
+            root.AppendChild(item);
+
+            XmlElement name = doc.CreateElement("Name");
+            name.InnerText = "Product A";
+            item.AppendChild(name);
+
+            XmlElement quantity = doc.CreateElement("Quantity");
+            quantity.InnerText = "10";
+            item.AppendChild(quantity);
+
+            XmlElement price = doc.CreateElement("Price");
+            price.InnerText = "19.99";
+            item.AppendChild(price);
+
+            XmlElement store = doc.CreateElement("Electronics");
+            store.InnerText = "iOS";
+            item.AppendChild(store);
+
+            doc.Save("data2.xml");
+
+            Console.WriteLine("XML file created successfully.");
+
+
+
+
+        }
+    }
+}
